44be079cb997888c7c5018fa2296964ddebdeb10
negimiku1227 <gamania1beanfun2@gmail.com>
2019-06-10 21:14:24 +0800

    v1.1 correct some mistake, page1 -> professor-ccc

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,6 @@
 function selectpage(num) {
     var page = document.getElementById("homepg");
-    if (num == 1) page.innerHTML = '<iframe width="1196" height="700" src="html/page1.html" frameborder="0" ></iframe>'
-    if (num == 2) page.innerHTML = '<iframe width="1200" height="700" src="html/page2.html" frameborder="0" ></iframe>'
+    if (num == 1) page.innerHTML = '<iframe width="1196" height="600" src="page1.html" frameborder="0" ></iframe>'
+    if (num == 2) page.innerHTML = '<iframe width="1196" height="600" src="page2.html" frameborder="0" ></iframe>'
+    if (num == 3) page.innerHTML = '<iframe width="1196" height="600" src="member.html" frameborder="0" ></iframe>'
   }
