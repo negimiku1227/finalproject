7f4a6f4a154b557a31d8fd163db4773cff64884e
negimiku1227 <gamania1beanfun2@gmail.com>
2019-06-11 01:21:50 +0800

    v1.11 change the templete, add some picture

--- js/page1.js
+++
@@ -1,5 +0,0 @@
-function selectpage(num) {
-    var page = document.getElementById("page1");
-    if (num == 1) page.innerHTML = '<iframe width="1196" height="600" src="professor-ccc.html" frameborder="0" ></iframe>'
-    if (num == 2) page.innerHTML = '<iframe width="1196" height="600" src="page2.html" frameborder="0" ></iframe>'
-  }
