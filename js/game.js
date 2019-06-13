28852b1faeaa9c28f21ec729df396e3aaca2e959
negimiku1227 <gamania1beanfun2@gmail.com>
2019-06-14 01:24:54 +0800

    v1.31 game - bug fixed finish, correcting other bug

--- js/game.js
+++ js/game.js
@@ -1,25 +1,30 @@
-var teacher = ["汪柏","洪智傑","陳建彰","張志勇"];
-var hint1 = ["sinolegend","手錶是假的","教我們組合語言的教授"];
-var hint2 = ["嗯，簡單！","那...乾我屁事？","我們上次上到哪啦？"];
-var hint3 = ["一手拿咖啡，一手MP4","他的職位不只是教授","他跟泉豪的姓氏是一樣的"];
-var hint4 = ["MALLOC","adjacency matrix","data structure"];
+var teacher;
+var hint1 = ["手錶是假的","sinolegend","教我們組合語言的教授"];
+var hint2 = ["楽天株式会社","嗯，簡單！","我們上次上到哪啦？"];
+var hint3 = ["一手咖啡，一手MP4","他的職位不只是教授","他跟泉豪的姓氏是一樣的"];
+var hint4 = ["RUST 語言","資工2C 班導其中一位","大學學習"];
+var hint5 = ["應用密碼學","我這一刀下去，半個班消失了","專題實驗：暫停收學生"];
 
 var nowhint = [];
 var hintmessage = "";
 var arrnum, score, question, hinttry = 0;
+var startgame = false;
 var ans;
 function show(){
+    startgame = true;
     document.getElementById("sbutton").src = "../pic/button_restart.png";
+    teacher = ["汪柏","洪智傑","陳建彰","蔡憶佳","黃心嘉"];
     nowhint = [];
     question = 0;
     arrnum = 0;
     score = 0;
     teacher.sort(
         function(){
-            return Math.random()>0.5?-1:1;
+            return Math.random()>0.5?-1:1; /* 將每個變數賦予0~1之隨機值， 並與0.5比較大小。
+                                              若>0.5則返回-1，表示此變數需排在下個變數的前面。反之返回1，此變數便須排在下個變數的後面*/
         }
     )
-    for(var i = 0; i < 4; i++){
+    for(var i = 0; i < 5; i++){
         for (var j = 0; j < 3; j++){
             if (teacher[i] == "汪柏"){
                 nowhint.push(hint1[j]);
@@ -30,14 +35,18 @@ function show(){
             if (teacher[i] == "陳建彰"){
                 nowhint.push(hint3[j]);
             }
-            if (teacher[i] == "張志勇"){
+            if (teacher[i] == "蔡憶佳"){
                 nowhint.push(hint4[j]);
+            }
+            if (teacher[i] == "黃心嘉"){
+                nowhint.push(hint5[j]);
             }
 
         }
         
     }
     console.log(nowhint);
+    document.getElementById("score").innerHTML = score;
     document.getElementById("hint").innerHTML = nowhint[0];
     
 }
@@ -45,7 +54,7 @@ function show(){
 function checkanswer(){
     
     ans = answer.value;
-    if (arrnum >= 12){
+    if (arrnum >= 15 || !startgame){
         return;
     }
 
@@ -61,7 +70,7 @@ function checkanswer(){
         alert("真可惜，答錯了（’へ’）");
     }
     document.getElementById("score").innerHTML = score;
-    if (arrnum < 12){
+    if (arrnum < 15){
         teacher.shift();
         document.getElementById("hint").innerHTML = nowhint[arrnum];
         document.getElementById("hint2").innerHTML = "";
@@ -78,6 +87,9 @@ function checkanswer(){
 }
 
 function addhint(){
+    if (arrnum >= 15 || !startgame){
+        return;
+    }
     if (hinttry >= 2){
         alert("提示用完啦");
     }
---
+++ js/page2.js
@@ -0,0 +1,7 @@
+function showrule(){
+    var size = document.getElementById("r");
+    size.style.fontSize = "30px";
+    document.getElementById("rule").innerHTML = "很簡單的小遊戲！按下START鍵開始遊戲！";
+    document.getElementById("rule2").innerHTML = "你需要依照畫面上的提示，猜出教授的全名！";
+    document.getElementById("rule3").innerHTML = "你需要依照畫面上的提示，猜出教授的全名！";
+}
